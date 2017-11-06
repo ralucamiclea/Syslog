371983440d8c18b27e70feb1265d14b510a08c0d
ralucamiclea <rmiclea02@gmail.com>
2017-11-06 15:24:47 +0200

    updated LogServer

--- src/dirtybits/LogServer.java
+++ src/dirtybits/LogServer.java
@@ -1,5 +1,7 @@
 package dirtybits;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,15 +9,14 @@ import java.util.Map;
 
 public class LogServer {
 	
-	private Map<LogThread, String> threads;
+	private Map<String,LogThread> threads;
 	private List<LogClient> clients;
 	private LogServerConfig config;
 	
 	public LogServer() {
-		threads = new HashMap<LogThread, String>();
+		threads = new HashMap<String,LogThread>();
 		clients = new ArrayList<LogClient>();
 		config = new LogServerConfig();
-		createFiles();
 	}
 	
 	/*Adds a new client to the list as long as the max number of clients
@@ -37,7 +38,16 @@ public class LogServer {
 		if(!clientExists(client))
 			return false;
 		
-		String filename = getFileName(client);
+		String filename;
+		try {
+			filename = Auxiliary.getFileName(config, log);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return false;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
 		LogThread thread = getThreadFor(filename);
 		
 		if(thread != null)
@@ -46,15 +56,6 @@ public class LogServer {
 			return false; //could not create thread
 		
 		return true;
-	}
-	
-	private void createFiles(){
-		//TODO: create log files according to the config file
-	}
-	
-	private String getFileName(LogClient client) {
-		//TODO: return corresponding filename
-		return "filename";
 	}
 	
 	private LogThread getThreadFor(String fileName){
